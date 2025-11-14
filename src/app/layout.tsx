0ce1eb1278616664505ed5f965cdce7a5de92f59
Hunter Coleman <hunter.coleman@scp.cy>
2025-11-14 16:58:07 -0500

    Added Tag Manager.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -184,6 +184,16 @@ export default function RootLayout({
   return (
     <html lang="en-US">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W9LCF6ZJ');`
+        }} />
+        {/* End Google Tag Manager */}
+        
         {/* DNS prefetch for external resources */}
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
         <link rel="dns-prefetch" href="//www.google-analytics.com" />
@@ -417,6 +427,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${tanHeadline.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9LCF6ZJ"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <MarketingHeader />
         {children}
         <Footer />
